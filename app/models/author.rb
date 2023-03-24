f3f6c7e3e43b8cbc338caa4761c9714310587918
fulepranali <pranali@navtech.io>
2023-03-24 12:43:08 +0530

    RE

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,4 @@
 class Author < ApplicationRecord
    has_many :books
+   validates_associated :books
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ApplicationRecord
     validates :name, presence: true
+    validates :terms_of_service, acceptance: { accept: 'yes' }
 end
